70f615c5fccf559ff0bc09bca835286102704b36
haidarrahman30 <haidarrahmans30@gmail.com>
2021-12-03 20:04:04 +0800

    push UI Auth Login and Signup Page

---
+++ app/src/main/java/com/example/decotuk_app_capstone/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.decotuk_app_capstone.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
