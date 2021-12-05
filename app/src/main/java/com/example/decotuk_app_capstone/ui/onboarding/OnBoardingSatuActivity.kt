aa44473510cca40487cea80797eb3ef229f6aafd
haidarrahman30 <haidarrahmans30@gmail.com>
2021-12-05 23:41:10 +0800

    update on boarding v1

---
+++ app/src/main/java/com/example/decotuk_app_capstone/ui/onboarding/OnBoardingSatuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.decotuk_app_capstone.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.decotuk_app_capstone.R
+
+class OnBoardingSatuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_satu)
+    }
+}
---
+++ app/src/main/java/com/example/decotuk_app_capstone/ui/onboarding/OnBoardingTigaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.decotuk_app_capstone.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardingTigaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_tiga)
+    }
+}
