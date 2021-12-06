95ce47d18ca13ab8eff0d992ec535edc302d043d
Katon Wahyudi <katonwahyudi@gmail.com>
2021-12-07 03:16:11 +0700

    onBoarding finishing, add Splashscreen, add fragment covid stat

--- app/src/main/java/com/example/decotuk_app_capstone/ui/onboarding/OnBoardingTigaActivity.kt
+++ app/src/main/java/com/example/decotuk_app_capstone/ui/onboarding/OnBoardingTigaActivity.kt
@@ -2,6 +2,7 @@ package com.example.decotuk_app_capstone.ui.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.decotuk_app_capstone.R
 
 class OnBoardingTigaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
