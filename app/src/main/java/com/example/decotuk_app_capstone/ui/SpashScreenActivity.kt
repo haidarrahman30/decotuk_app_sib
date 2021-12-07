f764e437c880ba68fe29031376a19b4e6f483c4b
haidarrahman30 <haidarrahmans30@gmail.com>
2021-12-07 13:03:33 +0800

    update logic splash screen

--- app/src/main/java/com/example/decotuk_app_capstone/ui/SpashScreenActivity.kt
+++ app/src/main/java/com/example/decotuk_app_capstone/ui/SpashScreenActivity.kt
@@ -3,12 +3,19 @@ package com.example.decotuk_app_capstone.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.decotuk_app_capstone.ui.auth.login.LoginActivity
 
 class SpashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }, 3000)
     }
 }
